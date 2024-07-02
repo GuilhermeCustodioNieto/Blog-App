4c4591786d8829af760b3c591783f183e0401c6e
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-07-02 00:29:55 -0300

    feat: criado todo o sistema de postagens do site, com o crud completo e com o front-end finalizado para essa parte

--- app.js
+++ app.js
@@ -1,17 +1,56 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 const mongoose = require("mongoose");
+const admin = require("./routes/admin");
+const path = require("path");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 // configs
+// Flash
+
+// Resto
 const app = express();
+
+app.use(
+  session({
+    secret: "blogapp",
+    resave: true,
+    saveUnitialized: true,
+  })
+);
+app.use(flash());
+
+//middleware
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("sucess_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.engine("handlebars", handlebars({ default: true }));
+app.engine("handlebars", handlebars.engine({ default: true }));
 app.set("view engine", "handlebars");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect("mongodb://localhost/blogapp")
+  .then(() => {
+    console.log(`Conectado ao mongo`);
+  })
+  .catch((err) => {
+    console.log(`Erro ao coectar, erro foi: ${err}`);
+  });
+
+// rotas
+app.use("/admin", admin);
 
 // Outros
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Servidor rodando em https://localhost:${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
